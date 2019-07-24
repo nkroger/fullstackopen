6ff8c685be88ee3f0ba9b5a83a6137f0669bc44e
nike <36738080+niketamido@users.noreply.github.com>
2019-07-24 21:13:55 +0300

    2.8 puhelinluettelo step3

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -3,14 +3,19 @@ import React, { useState } from 'react'
 
 const App = () => {
     const [ persons, setPersons ] = useState([
-        { name: 'Arto Hellas' }
+        {
+            name: 'Arto Hellas',
+            number: '040-1234567'
+        }
     ])
     const [ newName, setNewName ] = useState('')
+    const [ newNumber, setNewNumber ] = useState('')
 
-    const addName = (event) => {
+    const addPerson = (event) => {
         event.preventDefault()
         const newPerson = {
-            name: newName
+            name: newName,
+            number: newNumber
         }
 
         if (persons.map(p => p.name).includes(newName)) {
@@ -18,6 +23,7 @@ const App = () => {
         } else {
             setPersons(persons.concat(newPerson))
             setNewName('')
+            setNewNumber('')
         }
     }
 
@@ -25,17 +31,27 @@ const App = () => {
         setNewName(event.target.value)
     }
 
+    const handleNumberChange = (event) => {
+        setNewNumber(event.target.value)
+    }
+
     const people = () => persons.map(person =>
-        <>{person.name}<br /></>)
+        <>{person.name} {person.number}<br /></>)
 
     return (
         <div>
             <h2>Phonebook</h2>
-            <form onSubmit={addName}>
+            <form onSubmit={addPerson}>
                 <div>
                     name: <input 
                     value={newName}
                     onChange={handleNameChange}
+                    />
+                </div>
+                <div>
+                    number: <input
+                    value={newNumber}
+                    onChange={handleNumberChange}
                     />
                 </div>
                 <div>
