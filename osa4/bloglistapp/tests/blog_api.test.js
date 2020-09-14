14ff5b9ccf59a81e72aeda182f973fd2cec5f388
nkroger <niklas.kroger@gmail.com>
2020-09-14 22:25:52 +0300

    Update tests
    also removed unnecessary docker-compose file
    start dockerized mongo for tests with
    docker run -d --net=host mongo

--- osa4/bloglistapp/tests/blog_api.test.js
+++ osa4/bloglistapp/tests/blog_api.test.js
@@ -37,6 +37,33 @@ test('a specific blog is among the returned blogs', async () => {
   expect(contents).toContain(expectedBlog.title)
 })
 
+test('blogs should have an id field', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body[0].id).toBeDefined()
+})
+
+test('a valid blog post can be added', async () => {
+  const newBlog = {
+    title: 'A new blog post for testing',
+    author: 'Test Author Please Ignore',
+    url: 'https://www.notarealblog.post/atall'
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const expectedSize = helper.initialBlogs.length + 1
+  const blogsNow = await api.get('/api/blogs')
+  const titlesNow = blogsNow.body.map(b => b.title)
+  
+  expect(blogsNow.body).toHaveLength(expectedSize)
+  expect(titlesNow).toContain(newBlog.title)
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
