10601100eb907909c2b24a4354c4fcbd738784f5
Niklas Kröger <niklas.kroger@kuutomu.fi>
2022-09-14 18:30:51 +0300

    P5 ex 5.2

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import LoggedUser from './components/LoggedUser'
 
 const App = () => {
   const [blogs, setBlogs]               = useState([])
@@ -16,6 +17,15 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedInBlogUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
 
@@ -23,6 +33,10 @@ const App = () => {
       const user = await loginService.login({
         username, password
       })
+      window.localStorage.setItem(
+        'loggedInBlogUser', JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -32,6 +46,11 @@ const App = () => {
         setErrorMessage(null)
       }, 3000)
     }
+  }
+
+  const logout = () => {
+    setUser(null)
+    window.localStorage.removeItem('loggedInBlogUser')
   }
   
   const loginForm = () => (
@@ -72,7 +91,9 @@ const App = () => {
       { user === null ?
         loginForm() :
         <div>
-          <p>{user.name} logged in</p>
+          <LoggedUser
+            name={user.name}
+            logoutHandler={() => logout()} />
           {blogForm()}
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
