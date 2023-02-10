5174fa93dc4a9e219be6a1b1810e4256aca2bc1d
Niklas Kröger <niklas.kroger@columbiaroad.com>
2023-02-10 16:19:59 +0200

    p7 ex7 useCountry hook

--- osa7/routed-anecdotes/src/hooks/index.js
+++ osa7/routed-anecdotes/src/hooks/index.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-export const useField = (type, name) => {
+export const useField = (type) => {
   const [value, setValue] = useState('')
   
   const onChange = (event) => {
