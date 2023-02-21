782b20befddd7d5beb9ceba95ba4713d4be62867
Niklas Kröger <niklas.kroger@columbiaroad.com>
2023-02-21 15:33:03 +0200

    p9 ex4 add express and config

---
+++ osa9/tstest/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get("/hello", (_req, res) => {
+  res.send("Hello, Full Stack! 🌟")
+});
+
+const PORT = 3003;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+});
