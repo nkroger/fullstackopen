fa91d34d1bb16e4cfed1a465c5d8da725b8245a6
Niklas Kröger <niklas.kroger@columbiaroad.com>
2023-02-24 13:56:42 +0200

    p9 ex15 add more types

--- osa9/half-stack/src/components/Content.tsx
+++ osa9/half-stack/src/components/Content.tsx
@@ -1,22 +1,61 @@
-interface CoursePart {
-  name: string,
-  exerciseCount: number
-}
+import { CoursePart } from "../types";
 
 interface ContentProps {
   parts: CoursePart[];
 }
 
-const Part = (props: CoursePart) => {
+interface PartProps {
+  part: CoursePart;
+}
+
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
+
+// TODO check this
+const Part = (props: PartProps) => {
+  const part = props.part;
+  let attributes = null;
+  
+  switch (part.kind) {
+    case "basic":
+      attributes = <i>{part.description}</i>;
+      break;
+    case "background":
+      attributes = <>
+        <i>{part.description}</i><br />
+        {part.backroundMaterial}
+      </>
+      break;
+    case "group":
+      attributes = <>
+        Project exercises {part.groupProjectCount}
+      </>
+      break;
+    case "special":
+      attributes = <>
+      <i>{part.description}</i><br />
+      Required skills: {part.requirements.join(", ")}
+      </>
+      break;
+    default:
+      return assertNever(part);
+  }
+
   return (
-    <p>{props.name} {props.exerciseCount}</p>
+    <p>
+      {part.name} {part.exerciseCount}<br />
+      {attributes}
+    </p>
   )
 }
 
 const Content = (props: ContentProps) => {
   return (
       <>
-      {props.parts.map( (p, i) => <Part key={i} name={p.name} exerciseCount={p.exerciseCount} />)}
+      {props.parts.map( (p, i) => <Part key={i} part={p} />)}
       </>
   );
 };
