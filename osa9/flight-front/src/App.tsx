66f47e4f5cfa5ee42b26298204e2d8ac4edfa5f8
Niklas Kröger <niklas.kroger@columbiaroad.com>
2023-02-28 13:59:47 +0200

    p9 ex18 handle axios errors

--- osa9/flight-front/src/App.tsx
+++ osa9/flight-front/src/App.tsx
@@ -1,11 +1,22 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 import diaryService from "./services/diary";
 import { DiaryEntry, DiaryFormValues } from "./types";
 import Entry from "./components/Entry";
 import DiaryForm from "./components/DiaryForm";
 
+interface ErrorProps {
+  message: string;
+  duration?: number;
+}
+
 const App = () => {
   const [ entries, setEntries ] = useState<DiaryEntry[]>([]);
+  const [ errorMsg, setErrorMsg ] = useState<string>("");
+
+  const errorStyle = {
+    color: "red"
+  };
 
   useEffect(() => {
     const getEntries = async () => {
@@ -15,13 +26,33 @@ const App = () => {
     void getEntries();
   }, []);
 
+  const displayError = ({ message, duration=5 }: ErrorProps) => {
+    setErrorMsg(message);
+    setTimeout(() => {
+      setErrorMsg("");
+    }, duration * 1000);
+  };
+
   const addDiaryEntry = async (values: DiaryFormValues) => {
-    const entry = await diaryService.addEntry(values);
-    setEntries(entries.concat(entry));
+    try {
+      const entry = await diaryService.addEntry(values);
+      setEntries(entries.concat(entry));
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        if (error?.response?.data && typeof error?.response?.data === "string") {
+          displayError({ message: error.response.data.replace("Something went wrong. ", "") });
+        }
+      } else {
+        displayError({message: "Something went wrong!"});
+      }
+    }
   };
 
   return (
     <div>
+      { errorMsg &&
+        <div style={errorStyle}>{errorMsg}</div>
+      }
       <DiaryForm onSubmit={addDiaryEntry} />
       <h2>Diary entries</h2>
       {
