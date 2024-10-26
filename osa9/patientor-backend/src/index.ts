142715eb1de68a8628e828748b872a05284336c8
Niklas Kröger <niklas.kroger@kuutomu.fi>
2024-10-26 06:54:54 +0300

    install zod

--- osa9/patientor-backend/src/index.ts
+++ osa9/patientor-backend/src/index.ts
@@ -23,6 +23,7 @@ app.get("/api/ping", (_req, res) => {
 
 app.use("/api/diagnoses", diagnoseRouter);
 
+// TODO zod validation, middleware?
 app.use("/api/patients", patientsRouter);
 
 app.listen(PORT, () => {
